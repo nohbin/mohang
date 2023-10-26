f9be58b7c1cf816189a006071e5bb5c7efb84f46
Nohbin <nohbinmusic@gmail.com>
2023-10-26 19:47:40 +0900

    #4 Spring boot 초기화
    * Springboot - initial

---
+++ mohang/src/test/java/com/mohang/mohangboard/MohangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohang.mohangboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
