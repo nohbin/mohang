5b04c9b162c668c3681e5d55b234a2ad2ca9e35c
Nohbin <nohbinmusic@gmail.com>
2023-11-27 11:44:14 +0900

    #22 - UserAccount 수정 및  인증 권한 작업
    OAuth2 및 customSecurity 코드 작성

---
+++ src/test/java/com/example/mohang/config/SecurityConfigTest.java
@@ -0,0 +1,38 @@
+package com.example.mohang.config;
+
+import com.example.mohang.dto.UserAccountDto;
+import com.example.mohang.service.UserAccountService;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@Import(SecurityConfig.class)
+class SecurityConfigTest {
+
+    @MockBean
+    private UserAccountService userAccountService;
+
+    @BeforeTestMethod
+    public void securitySetup(){
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(),anyString(),anyString(),anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+                "unoTest",
+                "pw",
+                "uno-test@email.com",
+                "uno-test"
+        );
+    }
+
+}
