d4eec1d401d9e354a3a215edcb42f50d411e6bb2
Nohbin <nohbinmusic@gmail.com>
2024-01-15 14:14:05 +0900

    contextload 수정

--- src/test/java/com/example/mohang/MohangApplicationTests.java
+++ src/test/java/com/example/mohang/MohangApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.mohang;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class MohangApplicationTests {
 
     @Test
